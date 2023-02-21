307689392ed418853f477a2df9f0cc3e5e9bac6d
Thomas <thomas.lefloch@eisti.fr>
2023-02-21 23:13:28 +0100

    feat: adding support for firebase authentication

--- my-app/App.js
+++ my-app/App.js
@@ -1,84 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { Component, useState, useEffect } from 'react'
-import { Button } from 'react-native'
-import { AppRegistry, StyleSheet, Text, TextInput, View, Image } from 'react-native';
-import init from 'react_native_mqtt';
-import { AsyncStorage } from 'react-native';
+// In App.js in a new project
 
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { IconButton } from "@react-native-material/core";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Homepage from "./stack/Homepage";
+import DogBowl from "./stack/DogBowl";
+import LoadingScreen from "./stack/LoadingScreen";
+import Icon from "@expo/vector-icons/Ionicons";
+const Stack = createNativeStackNavigator();
 
-
-
-
-
-
-
-export default function App() {
-
-  const less = require('./assets/images/less.png');
-  const full = require('./assets/images/full.png');
-  const [choice, setSelected] = useState(less);
-
-  const options = {
-    host: 'test.mosquitto.org',
-    path: '0013a20012345678',
-    port: 8080,
-  };
-  
-  init({
-    size: 10000,
-    storageBackend: AsyncStorage,
-    defaultExpires: 1000 * 3600 * 24,
-    enableCache: true,
-    reconnect: true,
-    sync: {
-    }
-  });
-  
-  function onMessageArrived(message) {
-    console.log(message.payloadString);
-    let returnVal = parseInt(message.payloadString,10);
-    if(returnVal == 0){
-      setSelected(less);
-    }
-    else if(returnVal == 1200) {
-      setSelected(full);
-    }
-  }
-  
-  let client
-  try {
-    client = new Paho.MQTT.Client(options.host, options.port, options.path);
-  } catch (error) {
-    console.log(error);
-  }
-  
-  client.onMessageArrived = onMessageArrived;
-  client.connect({
-    onSuccess: () => {
-      client.subscribe(options.path);
-    },
-    timeout: 3,
-    onFailure: (message) => {
-      console.log('Connection failed: ' + message.errorMessage);
-    }
-  });
-
-
-
-  return (
-    <View style={styles.container}>
-      <Text>Votre gamelle est :</Text>
-      <Image source={choice}/>
-    </View>
-  );
+function resetAppState(navigation) {
+  console.log("Resetting state");
+  AsyncStorage.clear();
+  navigation.replace("Homepage");
 }
 
+export default function App() {
+  const [state, setState] = useState({
+    isLoading: true,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [isRegistered, setIsRegistered] = React.useState(false);
+
+  useEffect(() => {
+    AsyncStorage.getItem("dogBowlID").then((value) => {
+      if (value != null) {
+        setState({ isLoading: false });
+        setIsRegistered(true);
+      } else {
+        setState({ isLoading: false });
+      }
+    });
+  });
+
+  if (state.isLoading) {
+    return <LoadingScreen />;
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName={isRegistered ? "DogBowl" : "Homepage"}
+        >
+          <Stack.Screen
+            name="DogBowl"
+            component={DogBowl}
+            options={({ navigation }) => ({
+              headerTitle: "Doggeat",
+              headerRight: () => (
+                <IconButton
+                  onPress={() => resetAppState(navigation)}
+                  icon={(props) => <Icon {...props} name="exit-outline" />}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="Homepage"
+            component={Homepage}
+            options={{ title: "Accueil" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
